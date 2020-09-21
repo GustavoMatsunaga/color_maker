2678631f480e8ef600bddbc9cffd43ef971c9705
GustavoMatsunaga <63619508+GustavoMatsunaga@users.noreply.github.com>
2020-09-21 10:21:49 -0300

    FEATURE: Added the HEX type for the color generate

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,17 @@ var blueColor = document.querySelector("#color-blue")
 var opacity = document.querySelector("#opacity")
 
 // Text for colors
+
 var redText = document.querySelector(".text-color-red")
 var greenText = document.querySelector(".text-color-green")
 var blueText = document.querySelector(".text-color-blue")
 var opacityText = document.querySelector(".text-opacity")
 
 // Background color
+
 var colorBackground = document.querySelector(".card-background-color")
 var textBackground = document.querySelector(".card-background-text")
+var textBackgroundHex = document.querySelector("#hex")
 
 redColor.addEventListener("change", eventColorChange);
 greenColor.addEventListener("change", eventColorChange);
@@ -29,12 +32,39 @@ function eventColorChange(){
     greenText.value = greenColor.value
     blueText.value = blueColor.value
     opacityText.value = opacity.value;
+
+    // Var RGBA in a variable
+    var rgba = `rgba(${redColor.value}, ${greenColor.value}, ${blueColor.value}, ${opacity.value/100})`;
     
     // Changing the background using template literals
-    colorBackground.style.backgroundColor = `rgba(${redColor.value}, ${greenColor.value}, ${blueColor.value}, ${opacity.value/100})`;
+    colorBackground.style.backgroundColor = rgba;
 
     // Changing the text background using template literals
-    textBackground.textContent = `rgba  (${redColor.value}, ${greenColor.value}, ${blueColor.value}, ${opacity.value/100})`
+    textBackground.textContent = rgba
+
+    // Changing the text background using template literals
+    textBackgroundHex.textContent = rgba2hex(rgba)
 
 }
 })
+
+function rgba2hex(orig) {
+  var a, isPercent,
+    rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+    alpha = (rgb && rgb[4] || "").trim(),
+    hex = rgb ?
+    (rgb[1] | 1 << 8).toString(16).slice(1) +
+    (rgb[2] | 1 << 8).toString(16).slice(1) +
+    (rgb[3] | 1 << 8).toString(16).slice(1) : orig;
+
+  if (alpha !== "") {
+    a = alpha;
+  } else {
+    a = 01;
+  }
+  // multiply before convert to HEX
+  a = ((a * 255) | 1 << 8).toString(16).slice(1)
+  hex = `#${hex}${a}`;
+
+  return hex;
+}
